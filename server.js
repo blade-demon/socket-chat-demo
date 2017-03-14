3be0154e07113b2c885bb1a02996df4511bc7996
ziwei <ziwei@gamepoch.com>
2017-03-14 11:18:07 +0800

    Emmiting events

--- server.js
+++ server.js
@@ -3,9 +3,21 @@
  */
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
 
 http.listen(3000, function(){
