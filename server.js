c3eb0a915d7e05604829ba9dcd512ff2fc884a7e
ziwei <ziwei@gamepoch.com>
2017-03-14 11:02:45 +0800

    First commit

---
+++ server.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ziwei on 2017/3/14.
+ */
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
