789ff13c624fd59372356865b754cfa3abd266f8
ziwei <ziwei@gamepoch.com>
2017-03-14 11:27:54 +0800

    add broadcasting

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    //console.log('user disconnected');
   });
 
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+    //console.log('message: ' + msg);
   });
 });
 
